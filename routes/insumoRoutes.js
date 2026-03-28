7e2417bab6e3c91df3c28649cb9c0cf2ee0e3216
Ivan Castagnola <gium.oficial@gmail.com>
2026-03-28 20:27:30 -0300

    Fix: corregir mayúsculas en las rutas de los controladores

--- routes/insumoRoutes.js
+++ routes/insumoRoutes.js
@@ -1,8 +1,7 @@
 // Definimos los caminos
 const express = require('express');
 const router = express.Router();
-const insumoController = require('../controllers/insumoController'); // Verifica que la ruta al archivo sea correcta
-
+const insumoController = require('../controllers/InsumoController'); // Con I mayúscula
 // Revisa estas líneas, especialmente la del DELETE
 router.get('/', insumoController.obtenerInsumos);
 router.post('/', insumoController.crearInsumo);
